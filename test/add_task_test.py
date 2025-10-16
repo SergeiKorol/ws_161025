d89ff5e37185445a90bc3fa8496d05b052183d45
Taty-ermolova <taty-ermolova@yandex.ru>
2025-10-16 20:45:14 +0300

    tatiana

--- test/add_task_test.py
+++ test/add_task_test.py
@@ -7,3 +7,17 @@ def test_add():
     
     assert response.status_code == 202
     assert response_body['completed'] == False
+
+
+
+def test_add_new():
+    body = {"title":"general","completed":False}
+    response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
+    id = response.json()["id"]
+
+    body = {"completed":True}
+    requests.patch(f'https://todo-app-sky.herokuapp.com/{id}', json=body)
+
+    response = requests.get(f'https://todo-app-sky.herokuapp.com/{id}')
+    assert response.status_code == 200
+    assert response.json()['completed'] == True
